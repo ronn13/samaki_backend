b1f91c4a78e4c45dc91de1e5a8ca0d1f21628283
Ronnie Delyon <delyon.ronnie@gmail.com>
2019-06-20 23:52:26 +0300

    Changed Django Rest Framework pagination setting to 5 because phone screens be small

--- backend/settings.py
+++ backend/settings.py
@@ -127,7 +127,7 @@ REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
+    'PAGE_SIZE': 5,
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ]
